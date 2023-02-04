fe3dca8d2847d9f3127a94844baab60fdf1145be
M.R.A <reza.askary99@gmail.com>
2023-02-04 16:06:30 -0600

    session 1

--- session1.R
+++ session1.R
@@ -1,15 +1,24 @@
 library(dplyr)
 library(help=datasets)
 library(datasets)
-dat  = head(mtcars)
+dat <- head(mtcars)
 plot(dat$mpg, dat$hp)
 
 # asd #####
 
 many.numbers = 1:5
 # asd #####
-
+new.numbers= NULL
 for (i in many.numbers){
   print(i*2)
+  new.numbers[i] = 2*i
 }
 
+# with different vectors#################################
+short.vector = c(2,3,4)
+long.vector = short.vector + new.numbers
+# recycling in different sized vectorz
+LETTERS
+# indexin in R is similar to matlab
+LETTERS[c(3,20:25)]  # inclusion
+LETTERS[-c(3,20:25)] # exclusion
