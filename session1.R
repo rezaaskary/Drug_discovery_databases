cbb0b7451f91e83dc4f58dfa71577049fb815175
M.R.A <reza.askary99@gmail.com>
2023-02-04 14:48:46 -0600

    session 1

--- session1.R
+++ session1.R
@@ -1,1 +1,15 @@
 library(dplyr)
+library(help=datasets)
+library(datasets)
+dat  = head(mtcars)
+plot(dat$mpg, dat$hp)
+
+# asd #####
+
+many.numbers = 1:5
+# asd #####
+
+for (i in many.numbers){
+  print(i*2)
+}
+
