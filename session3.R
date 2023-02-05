8a1b6924eeca6aa0440e5e95f439b0aede98f2ab
reza <maskari@hawk.iit.edu>
2023-02-05 17:03:11 -0600

    practice

--- session3.R
+++ session3.R
@@ -1,3 +1,22 @@
 library(datasets)
 new_data = ChickWeight
 new_data[order(new_data$weight),]
+
+# merging datasets
+# this is similar to join in SQL
+# when we merge two datasets
+merged = merge(x=new_data$Chick,y=new_data$Diet)
+#####################################################33
+
+head(new_data)
+
+# adding variables
+new_data$newvariables = 1:nrow(new_data)
+
+
+# deleting
+new_data$newvariables = NULL
+new_data$newvariables_2 = ifelse(new_data$Chick > 12, 'L', 'S')
+
+#  anew approach
+new_data$newvariables_3 = with(new_data, ifelse(Chick > 12, T, F))
---
+++ session4.R
@@ -0,0 +1,5 @@
+library(datasets)
+data = chickwts
+
+c1c2 = cbind(data[,1],data[,2])
+r1r2 = rbind(data[,1],data[,2])
