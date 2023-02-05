82cbf4282f667300177fda317601bafe5bba7735
reza <maskari@hawk.iit.edu>
2023-02-05 12:50:07 -0600

    practice

--- session2.R
+++ session2.R
@@ -2,3 +2,16 @@ v1 = is.logical(3)
 v2 = as.numeric(5)
 v3 = as.integer(3.6)
 v4 = as.numeric(3.4)
+v5 = 5 + 3i
+v6= 'r'
+v7 = substr('I am good',start = 1,stop = 3)
+v8 = as.hexmode(23)
+# new ###################
+
+v9 = c(1,2,3)
+v10 = c(1,'4')
+length(v9)
+nchar('rty')
+
+v12 = paste(v9,c(3,7,8))
+
