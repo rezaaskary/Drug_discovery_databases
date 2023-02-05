c12bea57863b095cbce9d166bed26030578d0b69
reza <maskari@hawk.iit.edu>
2023-02-05 14:06:43 -0600

    practice

--- session2.R
+++ session2.R
@@ -8,10 +8,36 @@ v7 = substr('I am good',start = 1,stop = 3)
 v8 = as.hexmode(23)
 # new ###################
 
-v9 = c(1,2,3)
+v9 = c(1,2,3)  # this is vector
 v10 = c(1,'4')
 length(v9)
 nchar('rty')
 
 v12 = paste(v9,c(3,7,8))
+# making a liset
+l1 = list(4,5,5)
+l2 = list(bob=c(1,2),bill=c(T,F))
+names(l2) # getting names
+
+########
+f1 = c('a','b','c','a')
+factors = factor(f1)
+levels(factors)
+# factoes is nothing but the unique values of a vector
+levels(f1)
+# table is used to count the element sof a factor
+table(f1)
+barplot(table(f1))
+
+
+
+
+
+
+
+
+
+
+
+
 
