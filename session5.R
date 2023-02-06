5b06e5340767b61f813ad6d2d6a07d6485fc170e
reza <maskari@hawk.iit.edu>
2023-02-05 22:48:15 -0600

    SQL

--- session5.R
+++ session5.R
@@ -1,4 +1,6 @@
 library(datasets)
+library(DBI)
+library(RSQLite)
 data = ChickWeight
 # pair plot
 plot(data)
@@ -20,3 +22,14 @@ lots.of.letters[[1]][3]
 ############################################################33
 
 m1 = matrix(1:50,nrow = 5,ncol = 10,byrow = T)
+row_mean = apply(m1,2,mean)
+rowMeans(m1)
+colMeans(m1)
+#########################################################3
+# making a connection from mysqlite to our system
+mysqllite = dbConnect(RSQLite::SQLite(),'mySQLite.sqlite')
+
+data = ChickWeight
+
+
+
