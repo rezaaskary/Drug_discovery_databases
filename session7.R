e155b6860d4d90ab57db21ca16a22e8b7d1b0e6d
reza <maskari@hawk.iit.edu>
2023-02-07 21:58:46 -0600

    sampling

--- session7.R
+++ session7.R
@@ -1,4 +1,11 @@
 library(magrittr)
 
 
-1:23 |> fivenum() |> plot(1:5)
+3:23 |> fivenum() |T>  plot(1:5)
+myvar = 1:10
+# back and forth
+myvar %<>% fivenum()
+
+1:23 %>% fivenum() %>% LETTERS[.]
+
+
---
+++ session8.R
@@ -0,0 +1,14 @@
+library(datasets)
+data = ChickWeight
+numrt = rnorm(100,2,5)
+# cutting a vector to several split for analysis
+cut(numrt,3,labels = c('l','m','h'))
+cut(numrt,3,labels = F)
+cut(numrt, breaks = c(1,2,3))
+
+# spliting into lists
+splited = split(data,data$Chick)
+
+#
+askYesNo('?')
+
