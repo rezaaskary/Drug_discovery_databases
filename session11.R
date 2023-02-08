55d15a1a475eacbee5be1c0aaa857d90341670fd
reza <maskari@hawk.iit.edu>
2023-02-07 23:51:12 -0600

    sampling

---
+++ session11.R
@@ -0,0 +1,24 @@
+# how to work with inner and outer join
+
+# generating function that generates color
+my_clolors = colorRampPalette(c(colors()[1:10]))
+my_clolors(4)
+
+
+
+# if condision
+
+if(T){print('T')
+}else{print('F')
+}
+# this notation is useful for single arguments
+ifelse(T,print('T'),print('F'))
+
+
+# spine plots
+
+spineplot(ChickWeight$weight,ChickWeight$Diet)
+
+
+
+
